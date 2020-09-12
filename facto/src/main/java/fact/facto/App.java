0771e7386091e48d44e750046645d17518c1521b
sourabhbhardwaj <sourabhbhardwaj.3@gmail.com>
2020-09-12 23:31:43 +0530

    first commit

---
+++ facto/src/main/java/fact/facto/App.java
@@ -0,0 +1,13 @@
+package fact.facto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
